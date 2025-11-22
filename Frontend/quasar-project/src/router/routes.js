0cd8c9f4f343510926100f19e3bba68d8eb69107
Shirley Alfaro <22200097@ue.edu.pe>
2025-11-21 20:25:01 -0500

    Añadí el login

--- Frontend/quasar-project/src/router/routes.js
+++ Frontend/quasar-project/src/router/routes.js
@@ -5,8 +5,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
+      { path: '', redirect: '/login' },
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'digimons', component: () => import('pages/DigimonListPage.vue') }
+      { path: 'digimons', component: () => import('pages/DigimonListPage.vue') },
+      { path: 'login', component: () => import('pages/LoginPage.vue') }
     ]
   },
   {
